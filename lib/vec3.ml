8a48a4d5ab6f88b718c8ab1cd9f777531746eaf2
omnisci3nce <17525998+omnisci3nce@users.noreply.github.com>
2021-09-11 23:16:57 +1000

    vec3 add and tests

--- lib/vec3.ml
+++ lib/vec3.ml
@@ -1,5 +1,27 @@
+(* Vector 3 *)
 type t = {
   x: float; y: float; z: float
 }
 
-open
+let create x y z = { x; y; z }
+let zero = { x=0.; y=0.; z=0. }
+
+let add u v = {
+  x = u.x +. v.x;
+  y = u.y +. v.y;
+  z = u.z +. v.z
+}
+
+let subtract u v = {
+  x = u.x -. v.x;
+  y = u.y -. v.y;
+  z = u.z -. v.z
+}
+
+let multiply u s = {
+  x = u.x *. s;
+  y = u.y *. s;
+  z = u.z *. s;
+}
+
+let divide u s = multiply u (1. /. s)
---
+++ test/vec3.ml
@@ -0,0 +1,37 @@
+open Kosen
+
+let pprint_vec3 ppf (v :Vec3.t) =
+  let open Vec3 in
+  Fmt.pf ppf "%f %f %f\n" v.x v.y v.z
+
+let vec3_eq a b = (a = b)
+
+let vec3_testable = Alcotest.testable pprint_vec3 vec3_eq
+
+let test_add_positives () =
+  let u = Vec3.create 1. 1. 1.
+  and v = Vec3.create 1. 1. 1.
+  and w = Vec3.create 2. 2. 2. in
+  Alcotest.(check vec3_testable) "same" (Vec3.add u v) w
+
+let test_add_pos_neg () =
+  let u = Vec3.create 1. 1. 1.
+  and v = Vec3.create (-1.) (-1.) (-1.)
+  and w = Vec3.create 0. 0. 0. in
+  Alcotest.(check vec3_testable) "same" (Vec3.add u v) w
+
+let test_add_negatives () =
+  let u = Vec3.create (-1.) (-1.) (-1.)
+  and v = Vec3.create (-1.) (-1.) (-1.)
+  and w = Vec3.create (-2.) (-2.) (-2.) in
+  Alcotest.(check vec3_testable) "same" (Vec3.add u v) w
+
+let () =
+  let open Alcotest in
+  run "Vec3" [
+    "add", [
+      test_case "positives" `Quick test_add_positives;
+      test_case "positives and negatives" `Quick test_add_pos_neg;
+      test_case "negatives and negatives" `Quick test_add_negatives
+    ]
+  ]
