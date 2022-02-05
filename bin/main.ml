163f47ca389d5e5b6237b623f2b2afe3056ce42d
omniscient <17525998+omnisci3nce@users.noreply.github.com>
2022-02-05 15:59:56 +1100

    world hit

--- bin/main.ml
+++ bin/main.ml
@@ -1,25 +1,33 @@
 open Kosen
 open Kosen.Defaultscene
 
-(* if (world.hit(r, 0, infinity, rec)) {
-  return 0.5 * (rec.normal + color(1,1,1));
-} *)
+
 
 let ground = Sphere.create (Vec3.create 0. (-100.5) (-1.)) 100.
 let sphere1 = Sphere.create (Vec3.create 0. 0. (-1.)) 0.5
 let world = [ground; sphere1]
 
-let ray_color (r: Ray.t) =
-  let t = Sphere.hit r sphere1 in
-  match t with
+let rec world_hit t_min t_max (r: Ray.t) = function
+  | [] -> None
+  (* | h::[] -> hit_sphere h r t_min t_max *)
+  | h::t -> begin
+    match Sphere.hit r h with
+    | None -> world_hit t_min t_max r t
+    | Some hitrec -> begin
+      match world_hit t_min t_max r t with
+        | Some (next: Material.hit_record) -> if hitrec.t < next.t then Some hitrec else Some next
+        | None -> Some hitrec
+    end
+  end
+
+let ray_color (r: Ray.t) world =
+  match world_hit 0.0001 max_float r world with
   | None ->
     let unit_direction = Vec3.unit_vector r.direction in
     let t = 0.5 *. (unit_direction.y +. 1.0) in
-    Vec3.(((create 1. 1. 1.) *| (1.0 -. t)) +| ((create 0.5 0.7 1.0) *| t)
-  )
-  | Some hit_rec ->
-    Vec3.( (hit_rec.normal +| (Vec3.create 1. 1. 1.)) *| 0.5 )
-    
+    Vec3.(((create 1. 1. 1.) *| (1.0 -. t)) +| ((create 0.5 0.7 1.0) *| t))
+  | Some hitrec ->
+      Vec3.( (hitrec.normal +| (Vec3.create 1. 1. 1.)) *| 0.5 )
 
 let () = 
   let oc = open_out "image.ppm" in
@@ -38,7 +46,7 @@ let () =
           origin
           (lower_left_corner +| (horizontal *| u) +| (vertical *| v) -| origin) in
 
-      let color = ray_color r in
+      let color = ray_color r world in
       Color.write_color oc color
     done
   done;
